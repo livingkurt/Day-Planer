f6a76596021fbf7ac46ed0a6f5206bfeeae2fc1f
Kurt LaVacque <lavacquek@icloud.com>
2019-12-18 17:45:02 -0600

    Make it so everything lines up correctly, and that the icons show up on the buttons correctly too

--- scripts/script.js
+++ scripts/script.js
@@ -25,12 +25,15 @@ function create_containers() {
     //Create Row Div
     var row_container_e = $("<div>");
     row_container_e.attr("class", "row");
+    row_container_e.attr("style", "height: 100%;");
+    
     container_e.append(row_container_e);
     for (var i = 0; i < 9; i++){
         
         //Create Each row
         var row_e = $("<div>");
         row_e.attr("class", "col-md-12");
+        row_e.attr("style", "display: flex;");
         row_container_e.append(row_e);
 
         //Create Time label
@@ -43,19 +46,49 @@ function create_containers() {
             time_pm++
             label_e.text(time_pm + "PM");
         }
-        label_e.attr("class", "col-md-2");
+        label_e.attr("class", "hour col-md-2");
+
+        // label_e.attr("style", "top: -50px; ");
+        label_e.attr("style", "height: 100%;");
         row_e.append(label_e);
 
         //Create Text Area
         var textarea_e = $("<textarea>");
-        textarea_e.text("Daily Tasks");
-        textarea_e.attr("class", "col-md-10");
+        // textarea_e.text("Daily Tasks");
+
+        textarea_e.attr("class", "description hour present col-md-8");
+        textarea_e.attr("placeholder", "Daily Task");
+        textarea_e.attr("style", "height: 100%;");
         row_e.append(textarea_e);
+
+        //Create Save Button
+        var save_button_e = $("<button>");
+        // save_button_e.text("Daily Tasks");
+        save_button_e.attr("class", "saveBtn hour col-md-2");
+        save_button_e.attr("style", "height: 100%;");
+        // save_button_e.attr("style", "height: 69px; top: -3px;");
+        
+        row_e.append(save_button_e);
+
+        //Create Save Icon
+        var save_icon_e = $("<i>");
+        // save_button_e.text("Daily Tasks");
+        save_icon_e.attr("class", "far fa-save");
+        save_icon_e.attr("style", "font-size: 40px");
+        // save_button_e.attr("style", "height: 69px; top: -3px;");
+        
+        save_button_e.append(save_icon_e);
+
 
     }
 
 }
 
 create_containers();
+
+
+function create_row_div() {
+    
+}
 
 
